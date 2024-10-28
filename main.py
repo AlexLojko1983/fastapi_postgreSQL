79b25e9a693b792ea020986e9bf2b59f8ab22c26
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2024-10-28 16:34:09 +0300

    add mongodb app

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from typing import Union
-
-from fastapi import FastAPI
 from pydantic import BaseModel
+import contextlib
+from fastapi import FastAPI, HTTPException, Query
+from pymongo import MongoClient
+from bson import ObjectId
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
+client = MongoClient('mongodb://localhost:27017/')
+db = client['courses']
 
 class Item(BaseModel):
     name: str
@@ -24,3 +29,37 @@ async def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.get('/courses')
+async def get_courses(sort_by: str = 'date', domain=None):
+    for course in db.courses.find():
+        total = 0
+        count = 0
+        for chapter in course['chapters']:
+            with contextlib.suppress(KeyError):
+                total += chapter['rating']['total']
+                count += chapter['rating']['count']
+        db.courses.update_one({"id": course['id']}, {'$set':{"rating": {'total': total, 'count': count}}})
+
+    #Sort by
+    if sort_by == 'date':
+        sort_field = 'date'
+        sort_order = -1
+    elif sort_by == 'rating':
+        sort_field = 'rating.total'
+        sort_order = -1
+    else:
+        sort_field = 'name'
+        sort_order = 1
+
+    query = {}
+    if domain:
+        query['domain'] = domain
+
+    courses = db.courses.find(query, {'name': 1, 'date': 1, 'description': 1, 'rating': 1, 'id': 0}).sort(sort_field, sort_order)
+    return list(courses)
+
+
+
+
+
